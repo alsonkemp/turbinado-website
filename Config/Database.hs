ff6a4cde8b6853d346aa2a4744108ca9dc0e83c0
Alson Kemp <alson@alsonkemp.com>
2009-06-09 15:47:16 -0700

    Improving ORM (missing commit)

--- Config/Database.hs
+++ Config/Database.hs
@@ -1,18 +1,23 @@
 module Config.Database (
   databaseConnection,
+  ormAdapter
   ) where
 
 import System.Log.Logger
+import Database.HDBC
 
 -- Your favorite HDBC driver
-import Database.HDBC.PostgreSQL
+import Database.HDBC.ODBC
 
+import qualified Turbinado.Database.ORM.Adapters.MySQL as M
 import Turbinado.Environment.Types
 ----------------------------------------------------------------
 -- Database connection
 ----------------------------------------------------------------
--- databaseConnection :: Maybe (IO Connection)
+--databaseConnection :: Maybe (IO c)
 -- databaseConnection = Nothing
-databaseConnection = Just $ connectPostgreSQL "host=localhost dbname=turbinado user=turbinado password=turbinado"
+--databaseConnection = Just $ connectPostgreSQL "host=localhost dbname=turbinado user=turbinado password=turbinado"
+databaseConnection = Just $ connectODBC "DSN=turbinado;USER=user;password=password"
 
+ormAdapter = M.ormAdapter
 
